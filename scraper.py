d94cf3cebb31860ab822d7f1623714f5997c22ef
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2020-02-02 13:55:37 +1100

    Methodise scraper.py
    Scraper.py now more easy to understand and use, with custom row input and clear methods in code to prevent duplication

--- scraper.py
+++ scraper.py
@@ -8,58 +8,62 @@ NGOWorkBook = load_workbook(filename = 'NGOWorkBook.xlsx')
 NGOSheet = NGOWorkBook['NGOSheet']
 
 isContent = True
-rowNumber = 17
+rowNumber = int(input("Enter first row of data: "))
 
+def linkFinder(url, deliveryList, organisation, mode):
+    try:
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        Links = soup.body.find_all('a')
+
+        for Link in Links:
+            LinkString = str(Link).lower()
+            if mode == "Email" && LinkString.find('mailto') != -1:
+                deliveryList.append(Link['href'])
+            elif mode == "ContactURLs" && LinkString.find('contact') != -1:
+                if Link['href'].startswith('http'):
+                    deliveryList.append(Link['href'])
+                elif Link['href'].startswith('/'):
+                    deliveryList.append(url[:-1] + Link['href'])
+                else:
+                    deliveryList.append(url + Link['href'])
+    except:
+        print("Error on {0} mode of {1}".format(mode, organisation))
+
+def IFLGoogle(query):
+    urlGenerateds = search(cellValue, stop=1)
+    for urlGenerated in urlGenerateds:
+        return urlGenerated
+    
+def emailFormatter(emailList):
+    emailString = ''
+    emailList = set(emailList)
+    for email in emailList:
+        if email == emailList[-1]:
+            emailString += email[7:]
+            break
+        else:
+            emailString += (email[7:] + "; ")
+    return emailString
+    
 while isContent:
     cellValue = NGOSheet['A' + str(rowNumber)].value
     if cellValue:
-        try:
-            urlGenerateds = search(cellValue, stop=1)
-            for urlGenerated in urlGenerateds:
-                url = urlGenerated
-            response = requests.get(url)
+        URL = IFLGoogle(cellvalue)
+        contactURLs = []
+        
+        linkFinder(URL, contactURLs, cellValue, "ContactURLs")
+           
+        emails = []
 
-            soup = BeautifulSoup(response.text, "html.parser")
-            Links = soup.body.find_all('a')
+        for contactURL in contactURLS:
+            linkFinder(contactURL, emails, cellValue, "Email")
+        
+        emailString = emailFormatter(emails)
 
-            contactURLS = []
-
-            for Link in Links:
-                LinkString = str(Link).lower()
-                if LinkString.find('contact') != -1:
-                    if Link['href'].startswith('http'):
-                        contactURLS.append(Link['href'])
-                    elif Link['href'].startswith('/'):
-                        contactURLS.append(url[:-1] + Link['href'])
-                    else:
-                        contactURLS.append(url + Link['href'])
-
-            emails = []
-            emailString = ''
-
-            for contactURL in contactURLS:
-                print(contactURL)
-                contactResponse = requests.get(contactURL)
-                soup = BeautifulSoup(contactResponse.text, "html.parser")
-                contactLinks = soup.body.find_all('a')
-
-                for Link in contactLinks:
-                    LinkString = str(Link).lower()
-                    if LinkString.find('mailto') != -1:
-                        emails.append(Link['href'])
-
-                for email in emails:
-                    if email == emails[-1]:
-                        emailString += email[7:]
-                        break
-                    else:
-                        emailString += (email[7:] + "; ")
-
-            NGOSheet['C' + str(rowNumber)].value = emailString
-            NGOWorkBook.save('NGOWorkBook.xlsx')
-
-        except:
-            print("Error for: " + cellValue)
+        NGOSheet['C' + str(rowNumber)].value = emailString
+        NGOWorkBook.save('NGOWorkBook.xlsx')
             
         rowNumber += 1
 
